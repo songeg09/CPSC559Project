81a70c9d10825089f54e1a94a8c1e3c8caf6e10f
akara286 <alimohamedkara47@gmail.com>
2024-03-11 20:22:15 -0600

    tweaks

--- proxy.py
+++ proxy.py
@@ -6,8 +6,8 @@ app = Flask(__name__, static_folder='static')
 
 # List of replicas
 REPLICA_ADDRESSES = [
-    'http://localhost:5001/',
-    'http://localhost:5002/',
+    'http://127.0.0.1:5001/',
+    # 'http://localhost:5002/',
     # Add other replicas as needed
 ]
 
